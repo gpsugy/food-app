30e061a13586c34420e19b225c552471eebd9aac
Sugy Ghang <gpsugy@gmail.com>
2017-06-13 16:43:53 +0000

    Add testing

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,57 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { app, storage } from '../server';
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
 describe('Shopping List', function() {
-    it('should list items on get');
-    it('should add an item on POST');
+    it('should list items on GET', (done) => {
+        chai.request(app)
+            .get('/items')
+            .end((err, res) => {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.should.have.length(3);
+                res.body[0].should.be.a('object');
+                res.body[0].should.have.property('id');
+                res.body[0].should.have.property('name');
+                res.body[0].id.should.be.a('number');
+                res.body[0].name.should.be.a('string');
+                res.body[0].name.should.equal('Broad beans');
+                res.body[1].name.should.equal('Tomatoes');
+                res.body[2].name.should.equal('Peppers');
+                done();
+            });
+    });
+    it('should add an item on POST', (done) => {
+        chai.request(app)
+            .post('/items')
+            .send({'name': 'Kale'})
+            .end((err, res) => {
+                should.equal(err, null);
+                res.should.have.status(201);
+                res.should.be.json;
+                res.body.should.be.a('object');
+                res.body.should.have.property('name');
+                res.body.should.have.property('id');
+                res.body.name.should.be.a('string');
+                res.body.id.should.be.a('number');
+                res.body.name.should.equal('Kale');
+                storage.items.should.be.a('array');
+                storage.items.should.have.length(4);
+                storage.items[3].should.be.a('object');
+                storage.items[3].should.have.property('id');
+                storage.items[3].should.have.property('name');
+                storage.items[3].id.should.be.a('number');
+                storage.items[3].name.should.be.a('string');
+                storage.items[3].name.should.equal('Kale');
+                done();
+            });
+    });
     it('should edit an item on put');
     it('should delete an item on delete');
     it('should update an item that exists on POST');
@@ -14,3 +65,4 @@ describe('Shopping List', function() {
     it('should DELETE an ID that doesn\'t exist');
     it('should DELETE without an ID in the endpoint');
 });
+
