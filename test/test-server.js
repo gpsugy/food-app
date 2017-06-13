ce021b9140f05dfe48cb18d3ee35bae9bb147637
Sugy Ghang <gpsugy@gmail.com>
2017-06-13 02:15:06 +0000

    Create test stbus

---
+++ test/test-server.js
@@ -0,0 +1,16 @@
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on POST');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+    it('should update an item that exists on POST');
+    it('should have no data on POST');
+    it('should add something other than valid JSON on POST');
+    it('should PUT without an ID in the endpoint');
+    it('should PUT with different ID in the endpoint than the body');
+    it('should PUT to an ID that doesn\'t exist');
+    it('should PUT without body data');
+    it('should PUT with something other than valid JSON');
+    it('should DELETE an ID that doesn\'t exist');
+    it('should DELETE without an ID in the endpoint');
+});
