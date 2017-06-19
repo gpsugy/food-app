ed4f3891703bc49064de87fe07c1863a30d1a215
Sugy Ghang <gpsugy@gmail.com>
2017-06-19 12:45:14 -0400

    Request directly from yelp api from server

--- src/actions/Results.js
+++ src/actions/Results.js
@@ -36,8 +36,12 @@ export function fetchYelp() {
 		// }
 		let options = {
 			method: 'GET',
-		}
-		let url = 'https://gpsugy-food-app-server.herokuapp.com/businesses/search?term=Mexican Asian&latitude=42.25830870000001&longitude=-83.7312938';
+			headers: {
+				'Authorization': 'Bearer B71cf9eZq7_sj_X2p8axs-7rdYcqLR91pgWAb-xpfwQrpgEee9SRzIYaqkElP0xM1jhmQlw515nRnUGddYc7ilYPhKw0jpk_UVa7UDXV8XYW6kHtH3CUeNeg9VE4WXYx'
+			}
+		};
+		
+		let url = 'https://api.yelp.com/v3/businesses/search?term=Mexican Asian&latitude=42.25830870000001&longitude=-83.7312938';
 		return fetch(url, options)
 			.then(response => {
 				if (response.status < 200 || response.status >= 300) {
@@ -46,11 +50,11 @@ export function fetchYelp() {
 					throw error;
 				}
 				return response;
-			}).then(res => console.log(res))
-			.catch(err => console.log(err));
-			// }).then(response => response.json())
-			// .then(json => console.log(json))
-			// .catch(error => console.log(error))
+			}).then(response => response.json())
+			.then(json => console.log(json))
+			.catch(error => console.log(error))
+			// }).then(res => console.log(res))
+			// .catch(err => console.log(err));
 			
 			// .then(json => dispatch(fetchDescriptionSuccess(repo, json.description)))
 			// .catch(error => dispatch(fetchDescriptionError(repo, error)))
