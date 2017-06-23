178f9f72c98fe094ba910879cca9e3f3bd1e0010
Sugy Ghang <gpsugy@gmail.com>
2017-06-23 16:59:44 -0400

    Convert Business component to class

--- client/src/components/Business.js
+++ client/src/components/Business.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Business = ({ name, url, rating, review_count, price, categories, distance, image_url }) => (
-	<li className="biz">
-		<img src={image_url} alt={name} />
-		<div className="biz-main-info">
-			<h3>{name}</h3>
-			<div>
-				<div>Rated {rating} stars</div>
-				<span>{review_count} reviews</span>
-			</div>
-			<div>
-				<span>{price}</span>
-				<span>[categories here]</span>
-			</div>
-		</div>
-		<div className="distance-traits">
-			<span>{distance / 1609.344} miles away</span>
-		</div>
-	</li>
-)
+export default class Business extends Component {
+	constructor(props) {
+		super(props);
+	}
 
-export default Business;
+	render() {
+		const { name, url, rating, review_count, price, categories, distance, image_url } = this.props;
+		return (
+			<li className="biz">
+				<img src={image_url} alt={name} />
+				<div className="biz-main-info">
+					<h3>{name}</h3>
+					<div>
+						<div>Rated {rating} stars</div>
+						<span>{review_count} reviews</span>
+					</div>
+					<div>
+						<span>{price}</span>
+						<span>[categories here]</span>
+					</div>
+				</div>
+				<div className="distance-traits">
+					<span>{distance / 1609.344} miles away</span>
+				</div>
+			</li>
+		)
+	}
+}
