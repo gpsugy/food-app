96ca165c7ef1c2ece80d8923e6a5740b045075ac
Sugy Ghang <gpsugy@gmail.com>
2017-06-28 13:15:40 -0400

    Add functionality for different symbols appearing on click for rating sort

--- client/src/components/FilterBar.js
+++ client/src/components/FilterBar.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
 
+import { ASCENDING, DESCENDING, SORTING_TYPES } from '../utility/SortingTypes';
+
 export default class FilterBar extends Component {
 	constructor(props) {
 		super(props);
 
+		this.renderArrow = this.renderArrow.bind(this);
+	}
+
+	renderArrow(sorting) {
+		if (!sorting)
+			return null;
+		switch (SORTING_TYPES[sorting.rating_si]) {
+			case ASCENDING:
+				return <span>^</span>;
+			case DESCENDING:
+				return <span>.</span>;
+			default:
+				return null;
+		}
 	}
 
 	render() {
-		const { clickRating } = this.props;
+		const { sorting, clickRating } = this.props;
 		return (
 			<div>
 				<ul className="filter-list">
-					<li onClick={clickRating}><span>Rating ^</span></li>
+					<li onClick={clickRating}><span>Rating </span>{this.renderArrow(sorting)}</li>
 					<li><span>Price ^</span></li>
 					<li><span>Distance ^</span></li>
 				</ul>
--- client/src/containers/FilterBarContainer.js
+++ client/src/containers/FilterBarContainer.js
@@ -3,11 +3,12 @@ import { connect } from 'react-redux';
 import { sortBusinesses, toggleRatingSort } from '../actions/FilterBar';
 import FilterBar from '../components/FilterBar';
 
-// const mapStateToProps = (state, ownProps) => {
-// 	return {
-// 		sorting: state.businesses.sorting
-// 	};
-// }
+const mapStateToProps = (state, ownProps) => {
+	console.log(state.businesses.sorting);
+	return {
+		sorting: state.businesses.sorting
+	};
+}
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -19,7 +20,7 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 const FilterBarContainer = connect(
-	null,
+	mapStateToProps,
 	mapDispatchToProps
 )(FilterBar)
 
