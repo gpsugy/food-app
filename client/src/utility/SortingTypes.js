f5e66977d6772f740eaf1b3d9afed62d6965926c
Sugy Ghang <gpsugy@gmail.com>
2017-06-29 15:07:39 -0400

    Create function that defines filter types to actual miles

--- client/src/utility/SortingTypes.js
+++ client/src/utility/SortingTypes.js
@@ -18,3 +18,14 @@ export const DISTANCE_FILTER_TYPES = [
 	DRIVING,
 	ROAD_TRIP
 ];
+
+export function convertDistanceFI_ToMiles(distance_fi) {
+	switch (DISTANCE_FILTER_TYPES[distance_fi]) {
+		case WALKING:
+			return 1.0;
+		case DRIVING:
+			return 5.0;
+		default:
+			return 15.0;
+	}
+};
