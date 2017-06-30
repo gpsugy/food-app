671a671c5ae6ce7b4f2cbadd6b340aebd6db6482
Sugy Ghang <gpsugy@gmail.com>
2017-06-30 18:28:30 -0400

    Separate routes from server.js

--- server-test.js
+++ server-test.js
@@ -10,7 +10,7 @@ unirest.post('http://localhost:8080/test')
 .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
 .send({
 	id: 1,
-	name: 'Sugy Ghang'
+	name: 'Sugy Ghang',
 	email: 'gpsugy@gmail.com'
 })
 .end(function(res) {
@@ -21,7 +21,7 @@ unirest.post('http://localhost:8080/user')
 .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
 .send({
 	id: 1,
-	name: 'Sugy Ghang'
+	name: 'Sugy Ghang',
 	email: 'gpsugy@gmail.com'
 })
 .end(function(res) {
