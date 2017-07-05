dad7d63efe4d3fd61303bdb1a39feec31bde34c9
Sugy Ghang <gpsugy@gmail.com>
2017-07-05 17:19:07 -0400

    Create login view

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 
 import FoodTypeListContainer from '../containers/FoodTypeListContainer';
 import LocationFinderContainer from '../containers/LocationFinderContainer';
+import Login from './forms/Login';
 import ResultListContainer from '../containers/ResultListContainer';
-import Signup from './forms/signup';
+import Signup from './forms/Signup';
 
 const App = () => (
   <main>
@@ -12,6 +13,7 @@ const App = () => (
       <Route exact path='/' component={LocationFinderContainer} />
       <Route path='/foodTypes' component={FoodTypeListContainer} />
       <Route path='/results' component={ResultListContainer} />
+      <Route path='/login' component={Login} />
       <Route path='/signup' component={Signup} />
     </Switch>
   </main>
---
+++ client/src/components/forms/Login.js
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+
+export default class Signup extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: ''
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log('User logged in: ' + this.state.email + this.state.password);
+	}
+
+	render() {
+		const { email, password } = this.state;
+		const isEnabled = email.length > 0 && password.length > 0;
+		return (
+			<div>
+				<h1>Signup</h1>
+				<form onSubmit={this.handleSubmit}>
+					<div className="form-group">
+						<label>
+							Email: 
+							<input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+						</label>
+					</div>
+					<div>	
+						<label>
+							Password: 
+							<input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+						</label>
+					</div>
+					<input disabled={!isEnabled} type="submit" value="Submit" />
+				</form>
+				<p>Need an account? <Link to="/signup">Signup</Link></p>
+				<Link to="/">Home</Link>
+			</div>
+		)
+	}
+}
