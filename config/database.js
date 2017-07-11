63641710c78e596fe6a1773cdc2a4f356e865f67
Sugy Ghang <gpsugy@gmail.com>
2017-07-11 16:31:03 -0400

    Change mongoDB URI to non-deprecated environment variable

--- config/database.js
+++ config/database.js
@@ -4,6 +4,6 @@
 //                             process.env.MONGOLAB_URI :
 //                             'mongodb://localhost/food-app-dev');
 export const DATABASE_URL = (process.env.NODE_ENV === 'production' ?
-                            process.env.MONGOLAB_URI :
+                            process.env.MONGODB_URI :
                             'mongodb://localhost/food-app-dev');
 export const PORT = process.env.PORT || 8080;
