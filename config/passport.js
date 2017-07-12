ce9390cd1002abba57c6d210249434d55e02be2b
Sugy Ghang <gpsugy@gmail.com>
2017-07-12 16:04:27 -0400

    Implement JWT for login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,15 @@
 let LocalStrategy = require('passport-local').Strategy;
+let JwtStrategy = require('passport-jwt').Strategy;
+let ExtractJwt = require('passport-jwt').ExtractJwt;
 
 import User from '../models/User';
+
+let jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    secretOrKey: 'someRandomSecret'
+};
+
+export { jwtOptions };
 
 export default function passportConfig(passport) {
 	// =========================================================================
@@ -24,58 +33,93 @@ export default function passportConfig(passport) {
     });
 
 
+    passport.use(new JwtStrategy(jwtOptions, (payload, done) => {
+        console.log('payload received', payload);
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        User.findOne({ 'local.email': payload.email }, (err, user) => {
+            // if there are any errors, return the error
+            if (err)
+                return done(err, false);
+
+            // check to see if theres already a user with that email
+            if (user) {
+                console.log('found user with that email');
+                return done(null, false, { message: 'That email is already taken.' });
+            } else {
+
+                // if there is no user with that email
+                // create the user
+                var newUser = new User();
+
+                // set the user's local credentials
+                newUser.local.email = email;
+                newUser.local.password = newUser.generateHash(password);
+
+                // save the user
+                newUser.save((err) => {
+                    if (err)
+                        throw err;
+                    return done(null, newUser);
+                });
+            }
+
+        });
+    }))
+
+
     // =========================================================================
     // LOCAL SIGNUP ============================================================
     // =========================================================================
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-signup', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    (req, email, password, done) => {
+    // passport.use('local-signup', new LocalStrategy({
+    //     // by default, local strategy uses username and password, we will override with email
+    //     usernameField : 'email',
+    //     passwordField : 'password',
+    //     passReqToCallback : true // allows us to pass back the entire request to the callback
+    // },
+    // (req, email, password, done) => {
 
-        // asynchronous
-        // User.findOne wont fire unless data is sent back
-        process.nextTick(() => {
+    //     // asynchronous
+    //     // User.findOne wont fire unless data is sent back
+    //     process.nextTick(() => {
 
-	        // find a user whose email is the same as the forms email
-	        // we are checking to see if the user trying to login already exists
-	        User.findOne({ 'local.email' :  email }, (err, user) => {
-	            // if there are any errors, return the error
-	            if (err)
-	                return done(err);
+	   //      // find a user whose email is the same as the forms email
+	   //      // we are checking to see if the user trying to login already exists
+	   //      User.findOne({ 'local.email' :  email }, (err, user) => {
+	   //          // if there are any errors, return the error
+	   //          if (err)
+	   //              return done(err);
 
-	            // check to see if theres already a user with that email
-	            if (user) {
-	            	console.log('found user with that email');
-	                return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-	            } else {
+	   //          // check to see if theres already a user with that email
+	   //          if (user) {
+	   //          	console.log('found user with that email');
+	   //              return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+	   //          } else {
 
-	                // if there is no user with that email
-	                // create the user
-	                var newUser = new User();
+	   //              // if there is no user with that email
+	   //              // create the user
+	   //              var newUser = new User();
 
-	                // set the user's local credentials
-	                newUser.local.email = email;
-	                newUser.local.password = newUser.generateHash(password);
+	   //              // set the user's local credentials
+	   //              newUser.local.email = email;
+	   //              newUser.local.password = newUser.generateHash(password);
 
-	                // save the user
-	                newUser.save((err) => {
-	                    if (err)
-	                        throw err;
-	                    return done(null, newUser);
-	                });
-	            }
+	   //              // save the user
+	   //              newUser.save((err) => {
+	   //                  if (err)
+	   //                      throw err;
+	   //                  return done(null, newUser);
+	   //              });
+	   //          }
 
-	        });    
+	   //      });    
 
-        });
+    //     });
 
-    }));
+    // }));
 
     // =========================================================================
     // LOCAL LOGIN =============================================================
@@ -83,31 +127,31 @@ export default function passportConfig(passport) {
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
 
-    passport.use('local-login', new LocalStrategy({
-        // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
-        passwordField : 'password',
-        passReqToCallback : true // allows us to pass back the entire request to the callback
-    },
-    (req, email, password, done) => { // callback with email and password from our form
-        // find a user whose email is the same as the forms email
-        // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, (err, user) => {
-            // if there are any errors, return the error before anything else
-            if (err)
-                return done(err);
+    // passport.use('local-login', new LocalStrategy({
+    //     // by default, local strategy uses username and password, we will override with email
+    //     usernameField : 'email',
+    //     passwordField : 'password',
+    //     passReqToCallback : true // allows us to pass back the entire request to the callback
+    // },
+    // (req, email, password, done) => { // callback with email and password from our form
+    //     // find a user whose email is the same as the forms email
+    //     // we are checking to see if the user trying to login already exists
+    //     User.findOne({ 'local.email' :  email }, (err, user) => {
+    //         // if there are any errors, return the error before anything else
+    //         if (err)
+    //             return done(err);
 
-            // if no user is found, return the message
-            if (!user)
-                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+    //         // if no user is found, return the message
+    //         if (!user)
+    //             return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 
-            // if the user is found but the password is wrong
-            if (!user.validPassword(password))
-                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+    //         // if the user is found but the password is wrong
+    //         if (!user.validPassword(password))
+    //             return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
-            // all is well, return successful user
-            return done(null, user);
-        });
+    //         // all is well, return successful user
+    //         return done(null, user);
+    //     });
 
-    }));
+    // }));
 }
