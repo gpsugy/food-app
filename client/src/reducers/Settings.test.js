2ff2f01036c7c311ae65cc2a356c9cfd5226a000
Sugy Ghang <gpsugy@gmail.com>
2017-07-17 11:55:53 -0400

    Implement test for User Settings Update actions/reducer

--- client/src/reducers/Settings.test.js
+++ client/src/reducers/Settings.test.js
@@ -1,5 +1,9 @@
 import { account } from './';
-import { setUserFilters } from '../actions/Account';
+import {
+  setUserFilters,
+  updateSettingsRequest,
+  updateSettingsSuccess,
+} from '../actions/Settings';
 import { userToggleRatingSort } from '../actions/FilterBar';
 
 describe('(Reduxer) User Settings SORTING object changes', () => {
@@ -58,6 +62,61 @@ describe('(Reduxer) User Settings SORTING object changes', () => {
 		};
 		expect(
 			account(state, setUserFilters(dbUser))
+		).toEqual(expected);
+
+		it('Set new user filters on SET_USER_FILTERS', () => {
+		let state = {
+			default: {
+				filters: {
+					rating_si: 1,
+					prices: [1, null, 3, 4],
+					distance_fi: 1
+				}
+			},
+			isFetching: true
+		};
+
+		let filters = {
+			rating_si: 0,
+			prices: [null, null, null, 4],
+			distance_fi: 2
+		}
+		let expected = {
+			default: {
+				filters: {
+					rating_si: 0,
+					prices: [null, null, null, 4],
+					distance_fi: 2
+				}
+			},
+			isFetching: false
+		};
+		expect(
+			account(state, setUserFilters(filters))
+		).toEqual(expected);
+	});
+	});
+});
+
+describe('(Reduxer) Update Settings', () => {
+	it('Set isFetching on REQUEST', () => {
+		let expected = {
+			isFetching: true
+		};
+		expect(
+			account({}, updateSettingsRequest())
+		).toEqual(expected);
+	});
+
+	it('Set new user filters on SUCCESS', () => {
+		let state = {
+			isFetching: true
+		};
+		let expected = {
+			isFetching: false
+		};
+		expect(
+			account(state, updateSettingsSuccess())
 		).toEqual(expected);
 	});
 });
