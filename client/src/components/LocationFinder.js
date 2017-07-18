9574e101d7493a60a0182e6f37399918b8926e43
Sugy Ghang <gpsugy@gmail.com>
2017-07-18 14:29:44 -0400

    Render different nav menu items based on user's login status

--- client/src/components/LocationFinder.js
+++ client/src/components/LocationFinder.js
@@ -1,21 +1,25 @@
 import '../styles/LocationFinder.css';
+
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router'
-import React from 'react';
+import React, { Component } from 'react';
 
-const LocationFinder = ({ fetched, handleClick }) => (
-	<div>
-		{fetched ? (
-			<Redirect to="/foodTypes"/>
-		) : (
+export default class LocationFinder extends Component {
+	render() {
+		const { fetched, handleClick } = this.props;
+		return (
 			<div>
-				<h2>Hi! I am a Food Bot. I'm here to help you find what <em>you</em> want to eat!</h2>
-				<h2>First, I'll need to know where you are!</h2>
-				<button className="location-btn" type="button" onClick={handleClick}>Find My Location</button>
-				<Link to="/login" className="link">Log In</Link><Link to="/signup" className="link">Sign Up</Link>
+				{fetched ? (
+					<Redirect to="/foodTypes"/>
+				) : (
+					<div>
+						<h2>Hi! I am a Food Bot. I'm here to help you find what <em>you</em> want to eat!</h2>
+						<h2>First, I'll need to know where you are!</h2>
+						<button className="location-btn" type="button" onClick={handleClick}>Find My Location</button>
+						<Link to="/login" className="link">Log In</Link><Link to="/signup" className="link">Sign Up</Link>
+					</div>
+				)}
 			</div>
-		)}
-	</div>
-)
-
-export default LocationFinder;
+		)
+	}
+}
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,19 +5,38 @@ import { Link, Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
 
 export default class Navbar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.renderMenu = this.renderMenu.bind(this);
+	}
+
+	renderMenu(isLoggedOut, handleLogout) {
+		let menuItems = [];
+		if (isLoggedOut) {
+			menuItems.push(<li key='login'><Link to="/login">Login</Link></li>);
+			menuItems.push(<li key='signup'><Link to="/signup">Signup</Link></li>);
+		}
+		else {
+			menuItems.push(<li key='settings'><Link to="/settings">Settings</Link></li>);
+			menuItems.push(<li key='logout' onClick={handleLogout}>Log Out</li>);
+		}
+
+		return menuItems;
+	}
+
 	render() {
 		const { handleLogout, email } = this.props;
+		let isLoggedOut = (email === undefined || email == null);
 		return (
 			<nav>
 				<div className="user-container">
 					<i className="fa fa-user-circle fa-3x user-icon" aria-hidden="true"></i>
 					<ul className="menu-drop-down">
-						<li><Link to="/settings">Settings</Link></li>
-						<li onClick={handleLogout}>Log Out</li>
+						{this.renderMenu(isLoggedOut, handleLogout)}
 					</ul>
 				</div>
-				{// user has logged out
-					email == null ? (<Redirect to="/" />) : null}
+				{isLoggedOut ? (<Redirect to="/" />) : null}
 			</nav>
 		)
 	}
