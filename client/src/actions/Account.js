249a850cbcec53147081e9ad005cd2586bdc08b7
Sugy Ghang <gpsugy@gmail.com>
2017-07-19 12:15:26 -0400

    Remove unused dependency

--- client/src/actions/Account.js
+++ client/src/actions/Account.js
@@ -1,5 +1,3 @@
-import { redirect } from './Redirect';
-
 export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';
