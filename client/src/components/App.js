5c4e3d509e9cd72e70918a67538fa3f0f9469cd4
Sugy Ghang <gpsugy@gmail.com>
2017-07-24 23:59:33 -0400

    Add simplistic/subtle fading transitions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import { Route, Switch } from 'react-router-dom';
+import { RouteTransition } from 'react-router-transition';
 import React from 'react';
 
+import AlreadySignedup from '../components/errors/AlreadySignedup';
 import FoodTypeListContainer from '../containers/FoodTypeListContainer';
 import LocationFinderContainer from '../containers/LocationFinderContainer';
 import LoginContainer from '../containers/forms/LoginContainer';
@@ -9,23 +11,30 @@ import ResultListContainer from '../containers/ResultListContainer';
 import SettingsContainer from '../containers/private/SettingsContainer';
 import SignupContainer from '../containers/forms/SignupContainer';
 import UnAuthorizedContainer from '../containers/errors/UnAuthorizedContainer';
-import AlreadySignedup from '../components/errors/AlreadySignedup';
 
 const App = () => (
   <div>
     <NavbarContainer />
-    <main>
-      <Switch>
-        <Route exact path='/' component={LocationFinderContainer} />
-        <Route path='/foodTypes' component={FoodTypeListContainer} />
-        <Route path='/results' component={ResultListContainer} />
-        <Route path='/login' component={LoginContainer} />
-        <Route path='/signup' component={SignupContainer} />
-        <Route path='/settings' component={SettingsContainer} />
-        <Route path='/unauthorized' component={UnAuthorizedContainer} />
-        <Route path='/already' component={AlreadySignedup} />
-      </Switch>
-    </main>
+    <Route render={({ location, history, match }) => (
+        <RouteTransition
+          pathname={location.pathname}
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 1 }}
+          atActive={{ opacity: 1 }}
+        >
+          <Switch key={location.key} location={location}>
+            <Route exact path='/' component={LocationFinderContainer} />
+            <Route path='/foodTypes' component={FoodTypeListContainer} />
+            <Route path='/results' component={ResultListContainer} />
+            <Route path='/login' component={LoginContainer} />
+            <Route path='/signup' component={SignupContainer} />
+            <Route path='/settings' component={SettingsContainer} />
+            <Route path='/unauthorized' component={UnAuthorizedContainer} />
+            <Route path='/already' component={AlreadySignedup} />
+          </Switch>
+        </RouteTransition>
+      )}
+    />
   </div>
 )
 
