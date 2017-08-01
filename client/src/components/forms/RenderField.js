791d424f686eaa2e3584cb5829ffdc94ba9da5fc
Sugy Ghang <gpsugy@gmail.com>
2017-08-01 16:38:29 -0400

    Convert renderField to class and turn input red on invalid input

--- client/src/components/forms/RenderField.js
+++ client/src/components/forms/RenderField.js
@@ -1,16 +1,39 @@
-import React from 'react';
 import '../../styles/css/forms/RenderField.css';
 
-const renderField = ({ input, label, placeholder, type, meta: { touched, error, warning } }) => (
-	<div className="form-group">
-		<label>
-			{label}
-		</label>
-		<div>
-			<input {...input} placeholder={placeholder} type={type} />
-			{ touched && ((error && <p>{error}</p>) || (warning && <p>{warning}</p>))}
-		</div>
-	</div>
-)
+import React, { Component } from 'react';
 
-export default renderField;
+export default class renderField extends Component {	
+	constructor(props) {
+		super(props);
+
+		this.renderInput = this.renderInput.bind(this);
+	}
+
+	renderInput(input, placeholder, type, touched, error, warning) {
+		if (touched && (error || warning)) {
+			return <input {...input} className="invalidInput" placeholder={placeholder} type={type} />;
+		}
+		else {
+			console.log('never runo nce');
+			return <input {...input} placeholder={placeholder} type={type} />;	
+		}
+	}
+
+	render() {
+		const { input, label, placeholder, type, meta: { touched, error, warning } } = this.props;
+		let errorRender = touched && ((error && <p>{error}</p>) || (warning && <p>{warning}</p>));
+		return (
+			<div className="form-group">
+				<label>
+					{label}
+				</label>
+				<div>
+					{this.renderInput(input, placeholder, type, touched, error, warning)}
+					{ errorRender }
+				</div>
+			</div>
+		)
+	}
+}
+
+// fff7f4
