cc0b7d6c47588e3f144dfcbd664c3123884a0190
Sugy Ghang <gpsugy@gmail.com>
2017-08-18 14:04:46 -0400

    Extract separate AsyncButton container

--- client/src/components/LocationFinder.js
+++ client/src/components/LocationFinder.js
@@ -1,33 +1,15 @@
 import '../styles/css/LocationFinder.css';
 import '../styles/css/Spinner.css';
 
-import { CSSTransitionGroup } from 'react-transition-group';
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router'
 import React, { Component } from 'react';
 
-import Spinner from './Spinner';
+import AsyncButton from './AsyncButton';
 
 export default class LocationFinder extends Component {
-	constructor(props) {
-		super(props);
-
-		this.renderButton = this.renderButton.bind(this);
-	}
-
 	componentDidMount() {
 		this.props.onMount();
-	}
-
-	renderButton(handleClick, fetching, fetched) {
-		if (!fetching && !fetched) {
-			console.log('not fetching');
-			return(<button className="location-btn" key="location-btn" type="button" onClick={handleClick}>Find My Location</button>);
-		}
-		else {
-			console.log('fetching');
-			return(<Spinner key="spinner" />);
-		}
 	}
 
 	render() {
@@ -41,14 +23,7 @@ export default class LocationFinder extends Component {
 					<div>
 						<h2>Hi! I am a Food Bot. I'm here to help you find what <em>you</em> want to eat!</h2>
 						<h2>First, I'll need to know where you are!</h2>
-						<CSSTransitionGroup
-							component="div"
-							className="fade-container"
-							transitionName="fade"
-							transitionEnterTimeout={500}
-							transitionLeaveTimeout={300}>
-							{this.renderButton(handleClick, fetching, fetched)}
-						</CSSTransitionGroup>
+						<AsyncButton handleClick={handleClick} fetching={fetching} fetched={fetched} btnText="Find My Location"/>
 						{isLoggedOut ? (
 							<div className="account-links-container">
 								<Link to="/login" className="login-link">Log In</Link>
